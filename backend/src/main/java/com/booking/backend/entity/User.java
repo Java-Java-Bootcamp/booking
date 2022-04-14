429e91263e5ce5980dc3b53de7f6c7be26fbca65
d.borisov <dim.borisov937@gmail.com>
2022-04-14 16:45:52 +0300

    заменил h2 на postgres

--- backend/src/main/java/com/booking/backend/entity/User.java
+++ backend/src/main/java/com/booking/backend/entity/User.java
@@ -6,16 +6,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Builder
+@Table(name="\"user\"")
 public class User {
 
     @Id
