a06bec7a54fc4ad1f52c75746d279a79546898cc
d.borisov <dim.borisov937@gmail.com>
2022-04-14 14:14:33 +0300

    изменил поиск организаций по имени задача #8

--- bot/src/main/java/com/booking/bot/TelegramBot.java
+++ bot/src/main/java/com/booking/bot/TelegramBot.java
@@ -2,8 +2,8 @@ package com.booking.bot;
 
 import com.booking.bot.dto.BookingDto;
 import com.booking.bot.dto.OrganizationDto;
+import com.booking.bot.entity.Reservation;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,11 +14,9 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -46,18 +44,15 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             try {
                 handleMessage(update.getMessage());
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            } catch (JsonProcessingException e) {
+            } catch (TelegramApiException | JsonProcessingException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private Map<Long, String> statusChat = new HashMap<>();
+    private final Map<Long, String> statusChat = new HashMap<>();
 
     private void handleMessage(Message message) throws TelegramApiException, JsonProcessingException {
-
         if (message.hasText() && message.hasEntities()) {
             Optional<MessageEntity> commandEntity =
                     message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
@@ -65,82 +60,105 @@ public class TelegramBot extends TelegramLongPollingBot {
                 String command =
                         message.getText().substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
                 switch (command) {
-                    case "/find":
+                    case "/find" -> {
                         statusChat.put(message.getFrom().getId(), "/find");
-                        execute(
-                                SendMessage.builder()
-                                        .text("Введите имя организации")
-                                        .chatId(message.getChatId().toString())
-                                        .build()
-                        );
-                        break;
-                    case "/organizations":
+                    }
+                    case "/organizations" -> {
                         statusChat.put(message.getFrom().getId(), "/organizations");
-                        execute(
-                                SendMessage.builder()
-                                        .text("Список организаций:")
-                                        .chatId(message.getChatId().toString())
-                                        .build()
-                        );
-                        break;
-                    default:
-                        execute(
-                                SendMessage.builder()
-                                        .text("Сервис по бронированию.\n" +
-                                                "/find - поиск бронирования.\n" +
-                                                "/organizations - просмотр доступных организаций.")
-                                        .chatId(message.getChatId().toString())
-                                        .build()
-                        );
-                        break;
+                    }
+                    case "/help" -> sendDefaultMessage(message);
                 }
             }
         }
+        chatCommunication(message);
+    }
 
+    private void chatCommunication(Message message) throws TelegramApiException {
         WebClient client = WebClient.create("http://localhost:8080");
         String messageText = message.getText();
         Optional<String> messageString = parseString(messageText);
-        if (statusChat.get(message.getFrom().getId()) != null && statusChat.get(message.getFrom().getId()).equals("/find")) {
-            Mono<OrganizationDto[]> organizationDtoMono = client.get().uri("/organization?name={name}", messageString.get()).retrieve().bodyToMono(OrganizationDto[].class);
-            OrganizationDto[] organizationDto = organizationDtoMono.share().block();
-            OrganizationDto org = organizationDto[0];
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Название: " + org.name() + "\n" +
-                    "Расписание: " + org.schedule() + "\n" +
-                    "Кол-во столов: " + org.numbersOfTables() + "\n" +
-                    "Средний чек: " + org.averageCheck() + "\n" +
-                    "Рейтинг: " + org.rating() + "\n");
-            execute(
-                    SendMessage.builder()
-                            .text(stringBuilder.toString())
-                            .chatId(message.getChatId().toString())
-                            .build()
-            );
-            statusChat.put(message.getFrom().getId(), "free");
-        } else if (statusChat.get(message.getFrom().getId()).equals("/organizations")) {
-            Mono<OrganizationDto[]> organizationDtoMono = client.get().uri("organization?pageNo=0&pageSize=2&sortBy=name").retrieve().bodyToMono(OrganizationDto[].class);
-            OrganizationDto[] organizationDto = organizationDtoMono.share().block();
-            System.out.println(33);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (OrganizationDto o : organizationDto) {
-                stringBuilder.append(o.name() + "\n");
+
+        switch (statusChat.get(message.getFrom().getId())) {
+            case "/find" -> {
+                if (message.hasEntities()) {
+                    execute(
+                            SendMessage.builder()
+                                    .text("Введите название организации:")
+                                    .chatId(message.getChatId().toString())
+                                    .build()
+                    );
+                    break;
+                }
+                String page = "0";
+                String pageSize = "20";
+                String sortBy = "rate";
+                Mono<BookingDto[]> bookingDtoMono = client.get()
+                        .uri("/booking?pageNo={page}&pageSize={pageSize}&sortBy={sortBy}", page, pageSize, sortBy)
+                        .retrieve().bodyToMono(BookingDto[].class);
+                BookingDto[] bookingDtoList = bookingDtoMono.share().block();
+                StringBuilder outMessage = new StringBuilder();
+                for (BookingDto bookingDto : bookingDtoList) {
+                    if (bookingDto.organizationName().equals(messageString.get())) {
+                        outMessage.append(
+                                "Название: " + bookingDto.organizationName() + "\n" +
+                                        "Средний чек: " + bookingDto.bill() + "\n" +
+                                        "Рейтинг: " + bookingDto.rate() + "\n" +
+                                        "Доступные бронирования:\n");
+                        for (Reservation r : bookingDto.slotsOfReservation()) {
+                            outMessage.append(r.beginning() + "-" + r.ending() + " cвободно " + r.numbersOfTables() + " столов" + "\n");
+                        }
+                        execute(
+                                SendMessage.builder()
+                                        .text(outMessage.toString())
+                                        .chatId(message.getChatId().toString())
+                                        .build()
+                        );
+                        break;
+                    }
+                }
+                statusChat.put(message.getFrom().getId(), "free");
             }
+            case "/organizations" -> {
+                Mono<OrganizationDto[]> organizationDtoMono = client.get().uri("organization?pageNo=0&pageSize=2&sortBy=name").retrieve().bodyToMono(OrganizationDto[].class);
+                OrganizationDto[] organizationDto = organizationDtoMono.share().block();
+                StringBuilder outMessage = new StringBuilder();
+                outMessage.append("Список организаций:\n");
+                for (OrganizationDto o : organizationDto) {
+                    outMessage.append(o.name()).append("\n");
+                }
+                execute(
+                        SendMessage.builder()
+                                .text(outMessage.toString())
+                                .chatId(message.getChatId().toString())
+                                .build()
+                );
+                statusChat.put(message.getFrom().getId(), "free");
+            }
+            case "/free" -> {
+                execute(
+                        SendMessage.builder()
+                                .text("Выход в главное меню")
+                                .chatId(message.getChatId().toString())
+                                .build()
+                );
+            }
+            default -> sendDefaultMessage(message);
+        }
+    }
+
+    private void sendDefaultMessage(Message message) {
+        try {
             execute(
                     SendMessage.builder()
-                            .text(stringBuilder.toString())
+                            .text("""
+                                    Сервис по бронированию.
+                                    /find - поиск бронирования.
+                                    /organizations - просмотр доступных организаций.""")
                             .chatId(message.getChatId().toString())
                             .build()
             );
-            statusChat.put(message.getFrom().getId(), "free");
-        } else {
-            execute(
-                    SendMessage.builder()
-                            .text("Сервис по бронированию.\n" +
-                                    "/find - поиск бронирования.\n" +
-                                    "/organizations - просмотр доступных организаций.")
-                            .chatId(message.getChatId().toString())
-                            .build()
-            );
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
 
     }
