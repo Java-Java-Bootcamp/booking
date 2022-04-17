30a242a2d13feb886edf8e8a7c0f8d711784a2af
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-04-17 15:03:20 +0300

    fix by comment

--- backend/src/main/java/com/booking/backend/dto/BookingDto.java
+++ backend/src/main/java/com/booking/backend/dto/BookingDto.java
@@ -1,7 +1,5 @@
 package com.booking.backend.dto;
 
-import com.booking.backend.entity.Person;
-
 public record BookingDto(Long id,
                          PersonDto person) {
 }
--- backend/src/main/java/com/booking/backend/dto/ReservationDto.java
+++ backend/src/main/java/com/booking/backend/dto/ReservationDto.java
@@ -1,12 +1,10 @@
 package com.booking.backend.dto;
 
-import com.booking.backend.entity.Booking;
-import com.booking.backend.entity.SomeObject;
 
 public record ReservationDto(Long id,
                              Integer beginning,
                              Integer ending,
                              String date,
-                             SomeObject someObject,
-                             Booking booking) {
+                             SomeObjectDto someObjectDto,
+                             BookingDto bookingDto) {
 }
