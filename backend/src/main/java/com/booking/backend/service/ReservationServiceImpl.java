571e5c115ca388ad88e130df097df641e5bc328c
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-04-20 14:00:59 +0300

    fix

--- backend/src/main/java/com/booking/backend/service/ReservationServiceImpl.java
+++ backend/src/main/java/com/booking/backend/service/ReservationServiceImpl.java
@@ -1,27 +1,23 @@
 package com.booking.backend.service;
 
 import com.booking.backend.dto.ReservationDto;
-import com.booking.backend.entity.Booking;
-import com.booking.backend.entity.Reservation;
-import com.booking.backend.mapper.BookingMapper;
 import com.booking.backend.mapper.ReservationMapper;
-import com.booking.backend.repository.BookingRepository;
 import com.booking.backend.repository.ReservationRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
 
-    private ReservationRepository reservationRepository;
-    private ReservationMapper reservationMapper;
-    private BookingService bookingService;
-
+    private final ReservationRepository reservationRepository;
+    private final ReservationMapper reservationMapper;
+    private final BookingService bookingService;
 
     @Override
+    @Transactional
     public void updateWithNewBooking(ReservationDto reservationDto) {
         bookingService.addNewBooking(reservationDto.bookingDto());
         reservationRepository.save(reservationMapper.convertFromReservationDtoToReservation(reservationDto));
